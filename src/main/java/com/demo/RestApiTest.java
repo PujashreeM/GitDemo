9cad4a8017b7c67310dd3d9b6313dff10b13f94b
Pujashree <pujashreemallick11@gmail.com>
2026-03-02 15:46:12 +0530

    This is commit from Demo guy

---
+++ src/main/java/com/demo/RestApiTest.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.testng.annotations.Test;
+
+public class RestApiTest {
+    @Test
+    public void postJira(){
+        System.out.println("Post Jira");
+        System.out.println("Post Jira2");
+        System.out.println("Post Jira3");
+
+    }
+}
